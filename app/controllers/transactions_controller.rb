88c99417e76cf846387b7076fd9af4a5f02f6a79
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-15 09:22:01 -0500

    Fix Empty Transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,7 +5,7 @@ class TransactionsController < ApplicationController
 
   # GET /transactions or /transactions.json
   def index
-    @transactions = Transaction.includes(:group).all
+    @transactions = Transaction.includes(:group).order(created_at: :desc)
   end
 
   # GET /transactions/1 or /transactions/1.json
