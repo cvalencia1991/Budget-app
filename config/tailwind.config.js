156e841b2df175d54e7d141d829253897faaae8b
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-15 19:01:35 -0500

    Add Styles to the Pages

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      spacing: {
+        '128': '32rem',
+      }
     },
   },
   plugins: [
