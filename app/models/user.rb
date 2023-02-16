f6dd476be7ea7615f76ff26b012344a5e9347c64
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-16 18:10:58 -0500

    Delete confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :groups, dependent: :destroy
   has_many :transactions, dependent: :destroy
   validates :name, presence: true
